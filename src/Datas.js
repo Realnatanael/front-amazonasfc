a3b20371baf8bc8b6ed95e203117a6b5071aa500
Natanael Nascimento <Natanaelrealamazonas@gmail.com>
2024-04-04 03:35:02 -0400

    create data mock

---
+++ src/Datas.js
@@ -0,0 +1,30 @@
+export const news = [
+    {
+        title: "post de notícia",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: "https://www.aberje.com.brwp-content/uploads/2016/03/lorem-ipsum.jpg",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "post de design cartaz",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: "https://www.aberje.com.brwp-content/uploads/2016/03/lorem-ipsum.jpg",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "poster",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: "https://www.aberje.com.brwp-content/uploads/2016/03/lorem-ipsum.jpg",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "poster de jogador",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: "https://www.aberje.com.brwp-content/uploads/2016/03/lorem-ipsum.jpg",
+        likes: 2,
+        comments: 4
+    }
+]
